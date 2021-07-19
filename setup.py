c4bf96d1e631f7b942fbaeadbab15fa2bf4124aa
Thiago Teixeira <thiago@streamlit.io>
2021-07-18 18:32:52 -0700

    Up version to 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="st-annotated-text",
-    version="1.0.1",
+    version="2.0.0",
     author="Thiago Teixeira",
     author_email="me@thiagot.com",
     description="A simple component to display annotated text in Streamlit apps.",
