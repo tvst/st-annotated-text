baba30e03b54fb2248235388b4f2c6438bee925b
Thiago Teixeira <thiago.teixeira@snowflake.com>
2023-04-05 13:58:31 -0700

    Support for parameters, plus new styling.

--- annotated_text/util.py
+++ annotated_text/util.py
@@ -3,32 +3,13 @@ import html
 from htbuilder import H, HtmlElement, styles
 from htbuilder.units import unit
 
-# Only works in 3.7+: from htbuilder import div, span
-div = H.div
+# This works in 3.7+:
+# from htbuilder import span
+#
+# ...so we use the 3.7 version of the code above here:
 span = H.span
 
-# Only works in 3.7+: from htbuilder.units import px, rem, em
-px = unit.px
-rem = unit.rem
-em = unit.em
-
-# Colors from the Streamlit palette.
-# These are red-70, orange-70, ..., violet-70, gray-70.
-PALETTE = [
-    "#ff4b4b",
-    "#ffa421",
-    "#ffe312",
-    "#21c354",
-    "#00d4b1",
-    "#00c0f2",
-    "#1c83e1",
-    "#803df5",
-    "#808495",
-]
-
-OPACITIES = [
-    "33", "66",
-]
+import annotated_text.parameters as p
 
 def annotation(body, label="", background=None, color=None, **style):
     """Build an HtmlElement span object with the given body and annotation label.
@@ -75,38 +56,58 @@ def annotation(body, label="", background=None, color=None, **style):
     if color:
         color_style['color'] = color
 
-    if not background:
+    if background:
+        background_color = background
+    else:
         label_sum = sum(ord(c) for c in label)
-        background_color = PALETTE[label_sum % len(PALETTE)]
-        background_opacity = OPACITIES[label_sum % len(OPACITIES)]
+        background_color = p.PALETTE[label_sum % len(p.PALETTE)]
+        background_opacity = p.OPACITIES[label_sum % len(p.OPACITIES)]
         background = background_color + background_opacity
+
+    label_element = ""
+
+    if label:
+        separator = ""
+
+        if p.SHOW_LABEL_SEPARATOR:
+            separator = span(
+                style=styles(
+                    border_left=f"1px solid",
+                    opacity=0.1,
+                    margin_left=p.LABEL_SPACING,
+                    align_self="stretch",
+                )
+            ),
+
+        label_element = (
+            separator,
+            span(
+                style=styles(
+                    margin_left=p.LABEL_SPACING,
+                    font_size=p.LABEL_FONT_SIZE,
+                    opacity=p.LABEL_OPACITY,
+                )
+            )(
+                html.escape(label),
+            )
+        )
 
     return (
         span(
             style=styles(
+                display="inline-flex",
+                flex_direction="row",
+                align_items="center",
                 background=background,
-                border_radius=rem(0.33),
-                padding=(rem(0.125), rem(0.5)),
+                border_radius=p.BORDER_RADIUS,
+                padding=p.PADDING,
                 overflow="hidden",
+                line_height=1,
                 **color_style,
-                **style,
-            ))(
-
+                **style,)
+        )(
             html.escape(body),
-
-            span(
-                style=styles(
-                    padding_left=rem(0.5),
-                    text_transform="uppercase",
-                ))(
-                span(
-                    style=styles(
-                        font_size=em(0.67),
-                        opacity=0.5,
-                    ))(
-                    html.escape(label),
-                ),
-            ),
+            label_element,
         )
     )
 
